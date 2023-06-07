576ea57122155dc48285a8f234cc94135c600828
Toghiri <pietrovaccariinfo@gmail.com>
2023-06-07 16:13:28 +0200

    1.5.1.9 (moved scripts in js file)

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var countDownDate = new Date("Jun 21, 2023 00:00:00").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var giorni = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var ore = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minuti = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var secondi = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("countdown").innerHTML = giorni + "g " + ore + "h " + minuti + "m " + secondi + "s ";
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "0 giorni, buona fortuna!";
+    }
+}, 1000);
