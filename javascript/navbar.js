5be275e294400b51dd3f9b3fb2af516ebeff0dea
Toghiri <pietrovaccariinfo@gmail.com>
2023-07-07 23:10:21 +0200

    1.5.5.4 (responsive navbar)

---
+++ javascript/navbar.js
@@ -0,0 +1,17 @@
+const resposiveMenuButton = document.getElementById('responsiveMenuToggleButton');
+
+const navBarLinks = document.querySelector('.nav-links');
+
+resposiveMenuButton.addEventListener('click', () => {
+    navBarLinks.classList.toggle('open');
+    resposiveMenuButton.classList.toggle('open');
+})
+
+const allNavLinks = document.querySelectorAll('.nav-links li');
+
+allNavLinks.forEach(link => {
+    link.addEventListener('click', () =>{
+        navBarLinks.classList.remove('open');
+        resposiveMenuButton.classList.remove('open')
+    })
+} )
